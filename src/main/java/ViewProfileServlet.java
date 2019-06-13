bc28a6bc3c871f985c34ccd40da9bf853f107d83
Justin Reich <justinreich.dev@gmail.com>
2019-06-13 11:59:00 -0500

    add exercise walkthrough

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -8,6 +8,13 @@ import java.io.IOException;
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+
+        request.setAttribute("user", "bob");
+
         request.getRequestDispatcher("/profile.jsp").forward(request, response);
     }
 }
--- src/main/webapp/profile.jsp
+++ src/main/webapp/profile.jsp
@@ -9,7 +9,7 @@
     <jsp:include page="partials/navbar.jsp" />
 
     <div class="container">
-        <h1>Viewing your profile.</h1>
+        <h1>Viewing ${user}'s profile. He loves to hang out with ${user}.</h1>
     </div>
 
 </body>
