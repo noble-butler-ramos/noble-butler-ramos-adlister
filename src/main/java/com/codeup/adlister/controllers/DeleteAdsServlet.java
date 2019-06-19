8a1fd72a7f2ff3df37b936208c42ab88758a02c7
Josh Ramos <josh.r.938@gmail.com>
2019-06-19 11:24:43 -0500

    This is the delete Ads function

--- src/main/java/com/codeup/adlister/controllers/DeleteAdsServlet.java
+++ src/main/java/com/codeup/adlister/controllers/DeleteAdsServlet.java
@@ -1,7 +1,6 @@
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,16 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "controllers.AdsIndexServlet", urlPatterns = "/ads")
-public class AdsIndexServlet extends HttpServlet {
+@WebServlet(name = "controllers.DeleteAdsServlet", urlPatterns = "/delete")
+public class DeleteAdsServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("ads", DaoFactory.getAdsDao().all());
         request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
     }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Long id = Long.parseLong(request.getParameter("id"));
-
         DaoFactory.getAdsDao().deleteAds(id);
-        response.sendRedirect("/ads");
+        request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
     }
 }
